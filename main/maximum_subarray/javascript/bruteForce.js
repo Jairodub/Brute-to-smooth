ec3591797f3a83e0ff2963cbc4aed3c4751b1293
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-01 08:51:46 -0400

    Create bruteForce.js

---
+++ main/maximum_subarray/javascript/bruteForce.js
@@ -0,0 +1,14 @@
+var maxSubArray = function(nums) {
+    let maxSum, currentSum;
+    for(let i = 0; i < nums.length-1 ; i++){
+        if(i == 0) maxSum = nums[i];
+        currentSum = nums[i]
+        for(let j = i+1; j<nums.length-1; j++){
+            currentSum += nums[j];
+            if(currentSum > maxSum)maxSum = currentSum
+       
+        }
+    }
+     return maxSum;
+}
+ 
