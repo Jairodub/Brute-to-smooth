5965d7002c1559146d8b91eefdc0ce0fcf288997
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-03 12:33:46 -0400

    Update OofNN_bruteForce.py

--- main/maximum_subarray/python/OofNN_bruteForce.py
+++ main/maximum_subarray/python/OofNN_bruteForce.py
@@ -2,7 +2,7 @@ def maximum_sub_array (nums):
     for i in range(len(nums)):
         if i==0: max_sum = current_sum = nums[0]
         current_sum = nums[i]
-        for j in range (len(nums)):
+        for j in range (i+1, len(nums)):
             current_sum =current_sum + nums[j]
             if current_sum>max_sum: max_sum=current_sum
     return max_sum
