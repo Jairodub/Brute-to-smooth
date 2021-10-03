8e7d95148c4511476a1a9ea8e24059b7e9c95a99
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-03 12:28:50 -0400

    Update OofNN_bruteForce.js

--- main/maximum_subarray/javascript/OofNN_bruteForce.js
+++ main/maximum_subarray/javascript/OofNN_bruteForce.js
@@ -1,9 +1,9 @@
 var maxSubArray = function(nums) {
     let maxSum, currentSum;
-    for(let i = 0; i < nums.length-1 ; i++){
+    for(let i = 0; i < nums.length; i++){
         if(i == 0) maxSum = nums[i];
         currentSum = nums[i]
-        for(let j = i+1; j<nums.length-1; j++){
+        for(let j = i+1; j<nums.length; j++){
             currentSum += nums[j];
             if(currentSum > maxSum)maxSum = currentSum     
         }
