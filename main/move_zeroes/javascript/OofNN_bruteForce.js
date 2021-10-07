b820abac189dcdee80fc422481accc316c865aad
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-07 18:06:56 -0400

    Create OofNN_bruteForce.js

---
+++ main/move_zeroes/javascript/OofNN_bruteForce.js
@@ -0,0 +1,17 @@
+var moveZeroes = function(nums){
+  	if (nums.length>0){
+     	for (let i =0; i<nums.length-1; i++){
+          if(nums[i] == 0){
+           	console.log(nums[i]);
+            for(let j = i+1; j<nums.length; j++){
+              if(nums[j] != 0){
+                nums[i] = nums[j];
+                nums[j]=0;
+                break;
+                }
+          
+             }
+          }
+        }
+    }
+}
