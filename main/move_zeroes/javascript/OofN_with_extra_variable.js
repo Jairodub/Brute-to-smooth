68eaf3934797e3b7813cd7f6cf4a7df3c30868a7
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-07 19:23:17 -0400

    Update OofN_with_extra_variable.js

--- main/move_zeroes/javascript/OofN_with_extra_variable.js
+++ main/move_zeroes/javascript/OofN_with_extra_variable.js
@@ -1,13 +1,13 @@
 var moveZeroes = function(nums){
   	if (nums.length>0){
       	let lastZeroIndex;
-     	for (let i =0; i<nums.length; i++){  
-          if(nums[i]===0 && lastZeroIndex === undefined) lastZeroIndex = i; 
-          if (nums[i] != 0 && lastZeroIndex != undefined ){
-            nums[lastZeroIndex] = nums[i];
-            lastZeroIndex += 1;
-            nums[i] = 0;
+        for (let i =0; i<nums.length; i++){  
+            if(nums[i]===0 && lastZeroIndex === undefined) lastZeroIndex = i; 
+            if (nums[i] != 0 && lastZeroIndex != undefined ){
+              nums[lastZeroIndex] = nums[i];
+              lastZeroIndex += 1;
+              nums[i] = 0;
+            }
           }
-        }
     }
 }
