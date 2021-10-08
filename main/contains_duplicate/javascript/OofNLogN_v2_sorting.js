771bbfa60242717777d241e0e6a6288c09149b76
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-07 20:14:21 -0400

    Update OofNLogN_v2_sorting.js

--- main/contains_duplicate/javascript/OofNLogN_v2_sorting.js
+++ main/contains_duplicate/javascript/OofNLogN_v2_sorting.js
@@ -1,7 +1,7 @@
 var containsDuplicate = function(nums) {
     nums.sort();
-	for(let i = 0; i < nums.length-1; i++){
-    if (nums[i] === nums[i+1]) return true;
+    for(let i = 0; i < nums.length-1; i++){
+	if (nums[i] === nums[i+1]) return true;
     }
-  	return false;
+    return false;
 };
