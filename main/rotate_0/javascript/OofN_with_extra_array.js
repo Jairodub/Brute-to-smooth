4030d44970e508c17b4e27b43e53d9f31df4025b
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-13 14:32:24 -0400

    Update OofN_with_extra_array.js

--- main/rotate_0/javascript/OofN_with_extra_array.js
+++ main/rotate_0/javascript/OofN_with_extra_array.js
@@ -1,5 +1,10 @@
 var rotate = function(nums, k) {
   let numsTemp=[];
+  if(nums.length===0)return nums;
+  if(k/nums.length>1){
+		k=(k/nums.length - Math.floor(k/nums.length))*nums.length;
+    k=Math.round(k);
+	}
   for(let i = nums.length-k; i<nums.length; i++){
     numsTemp.push(nums[i]);
   }
