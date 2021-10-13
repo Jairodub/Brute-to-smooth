26a448425d4570857b0f382603523bf67881f7d6
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-13 14:34:09 -0400

    Update OofN_in_Place.js

--- main/rotate_0/javascript/OofN_in_Place.js
+++ main/rotate_0/javascript/OofN_in_Place.js
@@ -1,13 +1,13 @@
 var rotate = function(nums, k) {
     let numsTemp=[];
-    if(nums.length===1) return nums
+    if(nums.length===1) return nums;
     if(k/nums.length>1){
-        k=(k/nums.length - Math.floor(k/nums.length))*nums.length
-        k=Math.round(k)
+        k=(k/nums.length - Math.floor(k/nums.length))*nums.length;
+        k=Math.round(k);
    }
     for(let i = 0; i<nums.length; i++){
       if(i<k) {
-        numsTemp.push(nums[i])
+        numsTemp.push(nums[i]);
         nums[i]=nums[nums.length+i-k];
       } 
       if(i>=k){
