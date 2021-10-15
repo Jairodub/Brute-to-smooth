cd5cf296046cc08ae609c0112caa34ba26aab631
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-15 10:28:07 -0400

    Update hash_table.js

--- main/data_structures/hash_table/javascript/hash_table.js
+++ main/data_structures/hash_table/javascript/hash_table.js
@@ -1,1 +1,27 @@
+class HashTable{
+    constructor(size){
+        this.data = new Array (size);
+    }
+    _hash(key) {
+        let hash = 0;
+        for (let i =0; i < key.length; i++){
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length
+        }
+        return hash;
+      }
+      set(key, value){
+         let location=_hash(key);
+         this.data[location]=value;
+      }
+      get(key){
+          let location = _hash(key);
+          return data[location];
+      }
+}
 
+//Test Case
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000);
+console.log(myHashTable.get('grapes'));
+myHashTable.set('apples', 9)
+console.log(myHashTable.get('apples'));
