aea499bc5ffbecc885818722bdce6d5fe62310ed
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-16 16:03:23 -0400

    Update hash_table.js
    Make provisions for non-existing data, collisions, and updating data

--- main/data_structures/hash_table/javascript/hash_table.js
+++ main/data_structures/hash_table/javascript/hash_table.js
@@ -2,8 +2,6 @@ class HashTable{
     constructor(size){
         this.data = new Array (size);
     }
-    
-    //Hash function
     _hash(key) {
         let hash = 0;
         for (let i =0; i < key.length; i++){
@@ -11,20 +9,35 @@ class HashTable{
         }
         return hash;
       }
-    
       set(key, value){
-         let location=_hash(key);
-         this.data[location]=value;
+         let location =this._hash(key);
+         if(this.data[location]===undefined){
+           	this.data[location]=[];
+            this.data[location].push([key, value]);
+           	console.log(this.data[location][0]);
+         }else{
+            for(let i=0; i<this.data[location].length; i++){
+                if (key===this.data[location][i][0]){
+                    this.data[location][i]=[key,value];
+                    return;
+                }
+            }
+            this.data[location].push([key, value]);
+         }
+         
       }
       get(key){
-          let location = _hash(key);
-          return data[location];
+          let location = this._hash(key);
+          if(this.data[location] === undefined){
+              return undefined;
+          }else if (this.data[location].length ===1){
+              return this.data[location][0][1];
+          }else{
+             for(let i=0; i<this.data[location].length; i++){
+                  if (key===this.data[location][i][0]){
+                    return this.data[location][i][1];
+                  }
+              } 
+          }         
       }
 }
-
-//Test Cases
-const myHashTable = new HashTable(50);
-myHashTable.set('grapes', 10000);
-console.log(myHashTable.get('grapes')); //Expected output 10000
-myHashTable.set('apples', 9);
-console.log(myHashTable.get('apples')); //Expected output 9
