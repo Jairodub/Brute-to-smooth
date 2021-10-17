f8141a3e6b4b28d56aca43f46c2ad49a55af8760
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-17 08:30:01 -0400

    Delete main/two_sum/javascript/OofNLogN_hashing directory

--- main/two_sum/javascript/OofNLogN_hashing/OofNLogN_hashing.js
+++
@@ -1,6 +0,0 @@
-var twoSum = function(nums, target){
-	for (let i =0; i<nums.length; i++){
-      complimentIndex = nums.indexOf(target-nums[i]);
-      if (i != complimentIndex && complimentIndex!= -1) return [i, complimentIndex]
-    }
-}
