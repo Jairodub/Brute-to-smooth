75116905bcff92eaf29a13cb71e570e99f708cc0
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-17 12:05:23 -0400

    Update o_of_n_hashing.js

--- main/two_sum/javascript/o_of_n_hashing/o_of_n_hashing.js
+++ main/two_sum/javascript/o_of_n_hashing/o_of_n_hashing.js
@@ -1,6 +1,6 @@
 var twoSum = function(nums, target){
 	for (let i =0; i<nums.length; i++){
-      complimentIndex = nums.indexOf(target-nums[i]);
-      if (i != complimentIndex && complimentIndex!= -1) return [i, complimentIndex]
-    }
+      		complimentIndex = nums.indexOf(target-nums[i]);
+      		if (i != complimentIndex && complimentIndex!= -1) return [i, complimentIndex]
+    	}
 }
