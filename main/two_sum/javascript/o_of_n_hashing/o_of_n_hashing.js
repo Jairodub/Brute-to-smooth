1b1ac1d3f8d4be47d9956e91f9ff967a80dc5e26
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-19 12:15:39 -0400

    Update o_of_n_hashing.js

--- main/two_sum/javascript/o_of_n_hashing/o_of_n_hashing.js
+++ main/two_sum/javascript/o_of_n_hashing/o_of_n_hashing.js
@@ -1,6 +1,10 @@
 var twoSum = function(nums, target){
-	for (let i =0; i<nums.length; i++){
-      		complimentIndex = nums.indexOf(target-nums[i]);
-      		if (i != complimentIndex && complimentIndex!= -1) return [i, complimentIndex]
-    	}
+    let myMap = new Map();
+    for(let i =0; i<nums.length; i++){
+        myMap.set(nums[i],i);
+    }
+    for (let i =0; i<nums.length; i++){
+        complimentIndex = myMap.get(target-nums[i])
+        if (complimentIndex != undefined&& i != complimentIndex && complimentIndex!= -1) return [i, complimentIndex];
+    }
 }
