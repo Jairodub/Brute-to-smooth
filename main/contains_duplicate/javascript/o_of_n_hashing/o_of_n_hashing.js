6f194dc14b8648b094bb56f2bd683a1dac812297
Duncan Shitubi <Shyng2@yahoo.com>
2021-10-19 12:49:42 -0400

    Update o_of_n_hashing.js

--- main/contains_duplicate/javascript/o_of_n_hashing/o_of_n_hashing.js
+++ main/contains_duplicate/javascript/o_of_n_hashing/o_of_n_hashing.js
@@ -1,6 +1,8 @@
-var containsDuplicate = function(nums) {
-	for(let i = 0; i < nums.length; i++){
-    if (nums.includes(nums[i], i+1)) return true;
+var containsDuplicate = function(nums, target){
+    let myMap = new Map();
+    for(let i =0; i<nums.length; i++){
+        if(myMap.get(nums[i] != undefined)) return true
+        myMap.set(nums[i],i)
     }
-  	return false;
-};
+    return false;
+}
